6db130775e82295be9a66a96720401ce8de96447
Valera <gavrilow777@gmail.com>
2020-05-04 21:28:03 +0300

    finish with error

--- lib/components/quizz-body.dart
+++ lib/components/quizz-body.dart
@@ -7,6 +7,43 @@ class QuizzBody extends StatefulWidget {
 }
 
 class _QuizzBodyState extends State<QuizzBody> {
+  List<Icon> scoreKeeper = [
+//    Icon(
+//      Icons.check,
+//      color: Colors.green,
+//    ),
+//    Icon(
+//      Icons.check,
+//      color: Colors.green,
+//    ),
+//    Icon(
+//      Icons.close,
+//      color: Colors.red,
+//    ),
+//    Icon(
+//      Icons.check,
+//      color: Colors.green,
+//    ),
+//    Icon(
+//      Icons.close,
+//      color: Colors.red,
+//    ),
+  ];
+  List<String> questions = [
+    'Is JAVA object oriented language',
+    'Is JavaScript pure functional language',
+//    'Can you use clojures in C',
+//    'Has an abstract class instance',
+//    'Has singleton private constructor'
+  ];
+  List<bool> answers = [
+    true,
+    false,
+//    false,
+//    false,
+//    true
+  ];
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -15,37 +52,69 @@ class _QuizzBodyState extends State<QuizzBody> {
         Expanded(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Center(child: Text('Some question')),
+            child: Center(child: Text(questions[questionNumber])),
           ),
         ),
         Expanded(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: FlatButton(
-              child: Text('Btn one'),
-              color: Colors.black54,
-              onPressed: (){
-                print('One');
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FlatButton(
-              child: Text('Btn two'),
+              child: Text('True'),
               color: Colors.green,
               onPressed: () {
-                print('Two');
+                setState(() {
+                  if (questionNumber < questions.length ) {
+                    if (answers[questionNumber] == true) {
+                      scoreKeeper.add(Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ));
+                    } else {
+                      scoreKeeper.add(Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ));
+                    }
+                    questionNumber++;
+                    print(questionNumber);
+                    print('&&&&&&&&&&&&&&77');
+
+                  }
+                });
               },
             ),
           ),
         ),
         Expanded(
-          //TODO add list with checkboxes
-          child: Text('This is list'),
-        )
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FlatButton(
+              child: Text('False'),
+              color: Colors.red,
+              onPressed: () {
+                setState(() {
+                  if (questionNumber < questions.length - 1) {
+                    if (answers[questionNumber] == false) {
+                      scoreKeeper.add(Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ));
+                    } else {
+                      scoreKeeper.add(Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ));
+                    }
+                    questionNumber++;
+                  }
+                });
+              },
+            ),
+          ),
+        ),
+        Row(
+            //TODO add list with checkboxes
+            children: scoreKeeper)
       ],
     );
   }
